5f621447a0fa39eb2d2f34858551301a40a16cef
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-09-07 17:01:00 +0200

    chnages

--- Controllers/movieController.js
+++ Controllers/movieController.js
@@ -21,22 +21,39 @@ module.exports = {
 
   getAllMovies: async (req, res) => {
     try {
+      //**************************************  WAY 1  ************************************************ */
+      console.log(req.query);
       let queryStr = JSON.stringify(req.query);
+      console.log(queryStr);
+
+      /*
+      we replace gte,gt,lte or lt with $gte,$gt,$lte,$le but only exact words. 
+      So if another word contains gte or gt, we dont touch it.  Using \b helps us achieve this.
+      We add the 'g' -> so we replace every instance of the same word accros the string, not only 1 or the first.
+      */
       queryStr = queryStr.replace(
         /\b(gte|gt|lte|lt)\b/g,
         (match) => `$${match}`
       );
+      console.log(queryStr);
       const queryOBJ = JSON.parse(queryStr);
       console.log(queryOBJ);
 
-      const movies = await Movie.find(queryOBJ);
+      // const excludedFields = ["sort", "page", "limit", "fields"];
+      // const queryObj = { ...req.query };
+      // excludedFields.forEach((element) => {
+      //   delete queryObj[element];
+      // });
 
+      let query = Movie.find();
       //THIS IS THE QUERY WE ARE ACHIEVING ABOVE
       // const movies = await Movie.find({
       //   duration: { $gte: 90 },
       //   ratings: { $gte: 5 },
       //   price: { $lte: 100 },
       // });
+
+      //*****************************************  WAY 2  ****************************************** */
 
       //THIS IS THE MONGOOSE WAY USED TO FIND MOVIES BASED ON QUERIES: http://localhost:3003/api/movies/?duration=118&ratings=7&price=58
       // const movies = await Movie.find()
@@ -46,6 +63,26 @@ module.exports = {
       //   .gte(req.query.ratings)
       //   .where("price")
       //   .lte(+req.query.price);
+
+      //************************************** SORTING LOGIC  ************************************************ */
+      if (req.query.sort) {
+        const sortBy = req.query.sort.split(",").join(" ");
+        query = query.sort(sortBy);
+      } else {
+        query = query.sort("-createdAt");
+      }
+
+      //************************************** LIMITING FIELDS ************************************************ */
+      if (req.query.fields) {
+        // query.select('name duration price ratings')
+        const fields = req.query.fields.split(",").join(" ");
+        console.log(fields);
+        query = query.select(fields);
+      } else {
+        query = query.select("-__v");
+      }
+
+      const movies = await query;
 
       res.status(200).json({
         status: "Success",
@@ -61,6 +98,19 @@ module.exports = {
       });
     }
   },
+
+  // getAllMovies: async (req, res) => {
+  //   try {
+  //     let keyword = req.query.keyword;
+  //     const response = await Movie.aggregate([
+  //       { $match: { name: new RegExp(keyword, "i") } },
+  //     ]);
+
+  //     res.send(response);
+  //   } catch (error) {
+  //     res.status(500).send(error);
+  //   }
+  // },
 
   getMovie: async (req, res) => {
     try {
--- Models/movieModel.js
+++ Models/movieModel.js
@@ -33,6 +33,7 @@ const movieSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
+    select: false,
   },
   genres: {
     type: [String],
